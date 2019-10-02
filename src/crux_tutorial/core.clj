f00f3ef04270d0a16c1d9197dbf03327759b2ba5
Jose Luis Bras Marques da Cruz <jose-luis.cruz@serpro.gov.br>
2019-10-02 15:43:24 -0300

    datalog e bitemporatidade

--- src/crux_tutorial/core.clj
+++ src/crux_tutorial/core.clj
@@ -16,9 +16,11 @@
    :badges "SETUP"
    :cargo ["stereo" "gold fish" "slippers" "secret note"]})
 
-#_(crux/submit-tx crux [[:crux.tx/put manifest]])
-#_(crux/entity (crux/db crux) :manifest)
-#_(crux/submit-tx crux
+(crux/submit-tx crux [[:crux.tx/put manifest]])
+
+(crux/entity (crux/db crux) :manifest)
+
+(crux/submit-tx crux
                 [[:crux.tx/put
                   {:crux.db/id :commodity/Pu
                    :common-name "Plutonium"
@@ -39,7 +41,7 @@
                    :density 0.717
                    :radioactive false}]])
 
-#_(crux/submit-tx crux
+(crux/submit-tx crux
                 [[:crux.tx/put
                   {:crux.db/id :stock/Pu
                    :commod :commodity/Pu
@@ -70,7 +72,9 @@
                    :weight-ton 24.9}
                   #inst "2115-02-19T18"]])
 
-#_(crux/submit-tx crux
+(crux/entity (crux/db crux #inst "2115-02-19T18") :stock/Pu)
+
+(crux/submit-tx crux
                 [[:crux.tx/put
                   {:crux.db/id :stock/N
                    :commod :commodity/N
@@ -91,9 +95,178 @@
   (crux/submit-tx node
                   (vec (for [doc docs]
                          [:crux.tx/put doc]))))
-                         
 
-(crux/entity (crux/db crux #inst "2115-02-14") :stock/N)
+(crux/submit-tx crux
+                [[:crux.tx/put (assoc manifest :badges ["SETUP" "PUT"])]])
+
+(crux/entity (crux/db crux) :manifest)
+
+(crux/entity (crux/db crux #inst "2115-02-20") :stock/N)
+
+(def data
+  [{:crux.db/id :commodity/Pu
+    :common-name "Plutonium"
+    :type :element/metal
+    :density 19.816
+    :radioactive true}
+
+   {:crux.db/id :commodity/N
+    :common-name "Nitrogen"
+    :type :element/gas
+    :density 1.2506
+    :radioactive false}
+
+   {:crux.db/id :commodity/CH4
+    :common-name "Methane"
+    :type :molecule/gas
+    :density 0.717
+    :radioactive false}
+
+   {:crux.db/id :commodity/Au
+    :common-name "Gold"
+    :type :element/metal
+    :density 19.300
+    :radioactive false}
+
+   {:crux.db/id :commodity/C
+    :common-name "Carbon"
+    :type :element/non-metal
+    :density 2.267
+    :radioactive false}
+
+   {:crux.db/id :commodity/borax
+    :common-name "Borax"
+    :IUPAC-name "Sodium tetraborate decahydrate"
+    :other-names ["Borax decahydrate" "sodium borate" "sodium tetraborate" "disodium tetraborate"]
+    :type :mineral/solid
+    :appearance "white solid"
+    :density 1.73
+    :radioactive false}])
+
+(easy-ingest crux data)
+
+;; Datalog
+
+(crux/q (crux/db crux)
+        '{:find [element]
+          :where [[element :type :element/metal]]})
+
+(crux/q (crux/db crux)
+        {:find '[name]
+         :where '[[e :type :element/metal]
+                  [e :common-name name]]})
+
+(crux/q (crux/db crux)
+        '{:find [name rho]
+          :where [[e :density rho]
+                  [e :common-name name]]})
+
+(crux/q (crux/db crux)
+        {:find '[name]
+         :where '[[e :type t]
+                  [e :common-name name]]
+         :args [{'t :element/metal}]})
+
+(defn filter-type
+  [type]
+  (crux/q (crux/db crux)
+          {:find  '[name]
+           :where '[[e :type t]
+                    [e :common-name name]]
+           :args [{'t type}]}))
+
+(defn filter-appearance
+  [description]
+  (crux/q (crux/db crux)
+          {:find '[name IUPAC]
+           :where '[[e :common-name name]
+                    [e :IUPAC-name IUPAC]
+                    [e :appearance appearance]]
+           :args [{'appearance description}]}))
+                     
+(filter-type :element/metal)
+
+(filter-appearance "white solid")
+
+(crux/submit-tx
+ crux
+ [[:crux.tx/put
+   (assoc manifest :badges ["SETUP" "PUT" "DATALOG-QUERIES"])]])
+
+(crux/submit-tx
+ crux
+ [[:crux.tx/put
+   {:crux.db/id :consumer/RJ29sUU
+    :consumer-id :RJ29sUU
+    :first-name "Jay"
+    :last-name "Rose"
+    :cover? true
+    :cover-type :Full}
+   #inst "2114-12-03"]])
+
+(crux/submit-tx
+ crux
+ [[:crux.tx/put
+   {:crux.db/id :consumer/RJ29sUU
+    :consumer-id :RJ29sUU
+    :first-name "Jay"
+    :last-name "Rose"
+    :cover? true
+    :cover-type :Full}
+   #inst "2113-12-03" ;; Valid time start
+   #inst "2114-12-03"] ;; Valid time end
+
+  [:crux.tx/put
+   {:crux.db/id :consumer/RJ29sUU
+    :consumer-id :RJ29sUU
+    :first-name "Jay"
+    :last-name "Rose"
+    :cover? true
+    :cover-type :Full}
+   #inst "2112-12-03"
+   #inst "2113-12-03"]
+
+  [:crux.tx/put
+   {:crux.db/id :consumer/RJ29sUU
+    :consumer-id :RJ29sUU
+    :first-name "Jay"
+    :last-name "Rose"
+    :cover? false}
+   #inst "2112-06-03"
+   #inst "2112-12-02"]
+
+  [:crux.tx/put
+   {:crux.db/id :consumer/RJ29sUU
+    :consumer-id :RJ29sUU
+    :first-name "Jay"
+    :last-name "Rose"
+    :cover? true
+    :cover-type :Promotional}
+   #inst "2111-06-03"
+   #inst "2112-06-03"]])
+
+(crux/q (crux/db crux #inst "2115-07-03")
+        {:find '[cover type]
+         :where '[[e :consumer-id :RJ29sUU]
+                  [e :cover? cover]
+                  [e :cover-type type]]})
+
+(crux/q (crux/db crux #inst "2111-07-03")
+        {:find '[cover type]
+         :where '[[e :consumer-id :RJ29sUU]
+                  [e :cover? cover]
+                  [e :cover-type type]]})
+
+(crux/q (crux/db crux #inst "2112-07-03")
+        {:find '[cover type]
+         :where '[[e :consumer-id :RJ29sUU]
+                  [e :cover? cover]
+                  [e :cover-type type]]})
+
+(crux/submit-tx
+ crux
+ [[:crux.tx/put
+   (assoc manifest :badges ["SETUP" "PUT" "DATALOG-QUERIES" "BITEMP"])]])
 
 (defn -main
   "I don't do a whole lot ... yet."
